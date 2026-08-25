59ba8e52e7e376d57318d66ffc8ca3e11d619a7e
MOHAMMED SINAN MANSOOR <sinanmansooor@gmail.com>
2026-08-26 03:56:40 +0530

    fix:

--- portfolio_ai/settings.py
+++ portfolio_ai/settings.py
@@ -162,7 +162,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
